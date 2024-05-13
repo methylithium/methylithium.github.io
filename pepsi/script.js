854476c2e7750c2e92f3672128736ae38e946759
methylithium <118866387+methylithium@users.noreply.github.com>
2024-05-13 18:13:42 +0500

    Add files via upload

---
+++ pepsi/script.js
@@ -0,0 +1,8 @@
+function imgSlide(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+    const sec = document.querySelector('.sec');
+    sec.style.backgroundColor = color;
+}
